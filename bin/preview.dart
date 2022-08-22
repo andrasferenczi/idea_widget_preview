45f97dcf112167aaf238a7086b00faa3f36457af
Andras Ferenczi <a.ferenczi.andras@gmail.com>
2022-08-22 08:18:18 +0200

    Created project with dependencies

---
+++ bin/preview.dart
@@ -0,0 +1,5 @@
+import 'package:preview/preview.dart' as preview;
+
+void main(List<String> arguments) {
+  print('Hello world: ${preview.calculate()}!');
+}
---
+++ test/preview_test.dart
@@ -0,0 +1,8 @@
+import 'package:preview/preview.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
