9c64e553c7054e3feb006a45af3b94e27795c36e
Andras Ferenczi <a.ferenczi.andras@gmail.com>
2022-10-20 07:53:18 +0200

    WIP

--- lib/src/api/generated/api/kotlin_api.dart
+++ lib/src/api/generated/api/kotlin_api.dart
@@ -16,14 +16,66 @@ class KotlinApi {
 
   final ApiClient apiClient;
 
-  /// Register a client
+  /// Flutter client calls back using the endpoint to notify when the widgets have been rendered.
   ///
   /// Note: This method returns the HTTP [Response].
   ///
   /// Parameters:
   ///
-  /// * [RegisterClientRequest] registerClientRequest:
-  Future<Response> kotlinRegisterClientPostWithHttpInfo({ RegisterClientRequest? registerClientRequest, }) async {
+  /// * [FlutterPreviewReadyRequest] flutterPreviewReadyRequest (required):
+  Future<Response> kotlinFlutterPreviewReadyPostWithHttpInfo(FlutterPreviewReadyRequest flutterPreviewReadyRequest,) async {
+    // ignore: prefer_const_declarations
+    final path = r'/kotlin/flutter-preview-ready';
+
+    // ignore: prefer_final_locals
+    Object? postBody = flutterPreviewReadyRequest;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>['application/json'];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// Flutter client calls back using the endpoint to notify when the widgets have been rendered.
+  ///
+  /// Parameters:
+  ///
+  /// * [FlutterPreviewReadyRequest] flutterPreviewReadyRequest (required):
+  Future<Empty?> kotlinFlutterPreviewReadyPost(FlutterPreviewReadyRequest flutterPreviewReadyRequest,) async {
+    final response = await kotlinFlutterPreviewReadyPostWithHttpInfo(flutterPreviewReadyRequest,);
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'Empty',) as Empty;
+    
+    }
+    return null;
+  }
+
+  /// Dart client can register through this endpoint if it listens on a specific port
+  ///
+  /// Note: This method returns the HTTP [Response].
+  ///
+  /// Parameters:
+  ///
+  /// * [RegisterClientRequest] registerClientRequest (required):
+  Future<Response> kotlinRegisterClientPostWithHttpInfo(RegisterClientRequest registerClientRequest,) async {
     // ignore: prefer_const_declarations
     final path = r'/kotlin/register-client';
 
@@ -48,13 +100,65 @@ class KotlinApi {
     );
   }
 
-  /// Register a client
+  /// Dart client can register through this endpoint if it listens on a specific port
   ///
   /// Parameters:
   ///
-  /// * [RegisterClientRequest] registerClientRequest:
-  Future<Empty?> kotlinRegisterClientPost({ RegisterClientRequest? registerClientRequest, }) async {
-    final response = await kotlinRegisterClientPostWithHttpInfo( registerClientRequest: registerClientRequest, );
+  /// * [RegisterClientRequest] registerClientRequest (required):
+  Future<Empty?> kotlinRegisterClientPost(RegisterClientRequest registerClientRequest,) async {
+    final response = await kotlinRegisterClientPostWithHttpInfo(registerClientRequest,);
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'Empty',) as Empty;
+    
+    }
+    return null;
+  }
+
+  /// Update the view state so the plugin knows about it and can restore it.
+  ///
+  /// Note: This method returns the HTTP [Response].
+  ///
+  /// Parameters:
+  ///
+  /// * [UpdateViewStateRequest] updateViewStateRequest (required):
+  Future<Response> kotlinUpdateViewStatePostWithHttpInfo(UpdateViewStateRequest updateViewStateRequest,) async {
+    // ignore: prefer_const_declarations
+    final path = r'/kotlin/update-view-state';
+
+    // ignore: prefer_final_locals
+    Object? postBody = updateViewStateRequest;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>['application/json'];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// Update the view state so the plugin knows about it and can restore it.
+  ///
+  /// Parameters:
+  ///
+  /// * [UpdateViewStateRequest] updateViewStateRequest (required):
+  Future<Empty?> kotlinUpdateViewStatePost(UpdateViewStateRequest updateViewStateRequest,) async {
+    final response = await kotlinUpdateViewStatePostWithHttpInfo(updateViewStateRequest,);
     if (response.statusCode >= HttpStatus.badRequest) {
       throw ApiException(response.statusCode, await _decodeBodyBytes(response));
     }
